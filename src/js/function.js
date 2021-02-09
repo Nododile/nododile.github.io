e9284858bc453a6dd689ccf90518528b4fa97a8c
Thandile Nododile <nododilereply@gmail.com>
2021-02-09 16:37:31 +0200

    added pdf for payment

--- src/js/function.js
+++ src/js/function.js
@@ -1,3 +1,19 @@
 $("#myCarousel").carousel({
   interval: false
 });
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 100);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
